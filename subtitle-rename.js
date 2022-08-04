16ddf5713daabc83d6c9d4d7fd55942f69a8eb22
Zimomo333 <lzh120908717@gmail.com>
2022-08-04 21:51:19 +0800

    subtitle-rename up & low case

--- subtitle-rename.js
+++ subtitle-rename.js
@@ -5,15 +5,17 @@ let files = readFileRecursive("./");
 
 let movies = files.filter((file) => file.includes(".mkv"));
 
-let assS = files.filter((file) => file.includes(".简体&英文.ass"));
+let assS = files.filter((file) => file.includes(".srt"));
 
 movies.forEach((movie) => {
-    let exx = movie.match(/E\d\d/)[0];
-    let ass = assS.find((ass) => ass.includes(exx));
-    let newSubtitle = movie.replace("mkv", "ass");
-    fs.rename(ass, newSubtitle, function (err) {
-        if (err) {
-            throw err;
-        }
-    });
+    let exx = movie.toLowerCase().match(/s\d\de\d\d/)[0];
+    let ass = assS.find((ass) => ass.toLowerCase().includes(exx));
+    if(ass) {
+        let newSubtitle = movie.replace("mkv", "srt");
+        fs.rename(ass, newSubtitle, function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+    }
 });
